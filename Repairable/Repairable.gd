cfdc1052dbef9547e3358c84e308254a24c6a4c3
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2020-02-01 09:57:05 -0800

    Repairables bop at different rates

--- Repairable/Repairable.gd
+++ Repairable/Repairable.gd
@@ -1,11 +1,13 @@
 extends Spatial
 
-
-export(bool) var repaired = false
+export(bool) var repaired := false
+var repairTime : int
 
 func repair():
 	repaired = true
+	repairTime = OS.get_ticks_msec()
 
 func _process(delta):
 	if repaired:
-		scale.y = 1 + 0.2 * sin(TAU * OS.get_ticks_msec() / 1000)
+		var timeSinceRepair = OS.get_ticks_msec() - repairTime
+		scale.y = 1 + 0.2 * sin(TAU * timeSinceRepair / 700)
