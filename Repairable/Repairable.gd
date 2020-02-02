cc7b4435531353a37f883f13254ace7d1830c29e
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2020-02-01 21:57:14 -0800

    Now the thing requires multiple CDs

--- Repairable/Repairable.gd
+++ Repairable/Repairable.gd
@@ -2,13 +2,18 @@ extends Spatial
 class_name Repairable
 
 export(bool) var repaired := false
-var repairTime : int
+var repair_time : int
+
+var num_times_repaired := 0
+var num_repairs_needed := 10
 
 func repair():
-	repaired = true
-	repairTime = OS.get_ticks_msec()
+	num_times_repaired += 1
+	if num_times_repaired >= num_repairs_needed:
+		repaired = true
+		repair_time = OS.get_ticks_msec()
 
 func _process(delta):
 	if repaired:
-		var timeSinceRepair = OS.get_ticks_msec() - repairTime
+		var timeSinceRepair = OS.get_ticks_msec() - repair_time
 		scale.y = 1 + 0.2 * sin(TAU * timeSinceRepair / 700)
