1dc01f098e41e1e93b5a8a897c90c2d65d32a6b2
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2020-02-02 14:39:08 -0800

    Make a little animation play when a thing happens

--- UI/SpriteSwitcher.gd
+++ UI/SpriteSwitcher.gd
@@ -6,9 +6,22 @@ export(Texture) var second: Texture
 
 export(bool) var active := false
 
+var change_counter := 0.0
+var change_anim_time := 0.3
+var start_scale : Vector2
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	texture = first
+	start_scale = rect_scale
+
+func _process(delta):
+	if change_counter > 0:
+		change_counter -= delta
+		if change_counter < 0:
+			change_counter = 0
+		var anim_amt = change_counter / change_anim_time
+		rect_scale = (1 + 0.5 * anim_amt) * start_scale
 
 func _updateTexture():
 	if active:
@@ -18,6 +31,7 @@ func _updateTexture():
 
 func activate():
 	active = true
+	change_counter = change_anim_time
 	_updateTexture()
 
 func switch():
