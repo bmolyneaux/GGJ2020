a05fd40384e35aa55b0329908e9a91eb12f263f1
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2020-02-01 23:03:13 -0800

    Add text that lets you know how many more repairs you need to do

--- Repairable/Repairable.gd
+++ Repairable/Repairable.gd
@@ -7,13 +7,27 @@ var repair_time : int
 var num_times_repaired := 0
 var num_repairs_needed := 10
 
+func _ready():
+	$Sprite3D.texture = $Viewport.get_texture()
+	_updateText()
+
 func repair():
 	num_times_repaired += 1
 	if num_times_repaired >= num_repairs_needed:
 		repaired = true
 		repair_time = OS.get_ticks_msec()
+	_updateText()
 
 func _process(delta):
 	if repaired:
 		var timeSinceRepair = OS.get_ticks_msec() - repair_time
 		scale.y = 1 + 0.2 * sin(TAU * timeSinceRepair / 700)
+
+func _updateText():
+	if repaired:
+		$Sprite3D.visible = false
+		
+	var repairs_left := num_repairs_needed - num_times_repaired
+	var repair_string := "%s" % repairs_left
+	repair_string = repair_string.replace("0", "O")
+	$Viewport/RepairCount/RichTextLabel.text = repair_string
