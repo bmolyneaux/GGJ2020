f751604ac9ed8e9fd80a5cc8121e43100cb97dc3
Ben Molyneaux <bmolynea@google.com>
2020-02-04 19:48:39 -0800

    Dog tackle

--- Catcher/Catcher.gd
+++ Catcher/Catcher.gd
@@ -3,6 +3,10 @@ extends KinematicBody
 export(int) var index := 0
 export var acceleration := 10
 export var max_speed := 200
+export var tackle_speed_scale := 2.0
+
+export(Curve)  var tackle_speed_curve
+
 # TODO: Gravity should be an acceleration
 export var gravity_speed := -150
 var speed := Vector2(0, 0)
@@ -15,7 +19,7 @@ func _ready():
 	$PuppyDog.get_node("AnimationPlayer").play("Walk")
 
 func _physics_process(delta):
-	var desired_speed = input * max_speed
+	var desired_speed = input * max_speed * get_diving_speed_multiplier()
 	
 	speed = lerp(speed, desired_speed, 0.2)
 	
@@ -26,10 +30,13 @@ func _physics_process(delta):
 	$PuppyDog.rotate_x(-0.4)
 	
 	var is_moving = actual_movement.length_squared() > 0.01
-	if is_moving:
-		$PuppyDog.get_node("AnimationPlayer").play("Walk")
+	if is_tackling():
+		pass
 	else:
-		$PuppyDog.get_node("AnimationPlayer").play("Idle")
+		if is_moving:
+			$PuppyDog.get_node("AnimationPlayer").play("Walk")
+		else:
+			$PuppyDog.get_node("AnimationPlayer").play("Idle")
 	$RunParticles.emitting = is_moving
 
 	var bodies = $Area.get_overlapping_bodies()
@@ -42,12 +49,23 @@ func _physics_process(delta):
 			repairer.get_caught()
 			num_caught += 1
 
+func get_diving_speed_multiplier():
+	if not is_tackling():
+		return 1
+	var anim_player = $PuppyDog.get_node("AnimationPlayer")
+	var anim_amt = anim_player.get_current_animation_position() / anim_player.get_current_animation_length()
+
+	return tackle_speed_curve.interpolate(anim_amt) * tackle_speed_scale
+
+func is_tackling():
+	return $PuppyDog.get_node("AnimationPlayer").get_current_animation() == "Tackle" and $PuppyDog.get_node("AnimationPlayer").is_playing()
+
 func set_input(value : Vector2):
-	input = value
+	if not is_tackling():
+		input = value
 
 func main_action():
-	# Nothing yet.
-	pass
+	$PuppyDog.get_node("AnimationPlayer").play("Tackle", -1, 0.75)
 
 func cancelly_action():
 	pass
