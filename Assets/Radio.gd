651a36af431ed0b57b42fc77fffe933c8899e8c6
Ben <bmolynea@gmail.com>
2020-02-04 21:43:10 -0800

    Ensure particle shader is compiled at start

--- Assets/Radio.gd
+++ Assets/Radio.gd
@@ -1,5 +1,10 @@
 extends Spatial
 
+func _ready():
+	# The particles are set to emitting in the scene so that the shader is
+	# compiled on startup
+	$CPUParticles.emitting = false
+	$CPUParticles2.emitting = false
 
 func start_animation():
 	$Bounce.get_animation("Bounce").loop = true
